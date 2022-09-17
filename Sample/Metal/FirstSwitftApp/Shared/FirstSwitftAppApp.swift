e96cdd980fa941fe04d7d6b824b6c9aed3cc34fe
inwonn <103662424+inwonn@users.noreply.github.com>
2022-09-17 20:20:51 +0900

    충돌 구현
    충돌 컴포넌트 추가
    충돌 시 버튼 활성화

---
+++ Sample/Metal/FirstSwitftApp/Shared/FirstSwitftAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstSwitftAppApp.swift
+//  Shared
+//
+//  Created by 황인원 on 2022/09/15.
+//
+
+import SwiftUI
+
+@main
+struct FirstSwitftAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
